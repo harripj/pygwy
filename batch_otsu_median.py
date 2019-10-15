5b127a0d635f0e84e1ba13cd6b2913c60e720f4f
Paddy <pj.harrison31@gmail.com>
2019-10-15 22:51:51 +0100

    Update batch functions.

---
+++ batch_otsu_median.py
@@ -0,0 +1,32 @@
+"""
+Created on Tue Nov  6 16:55:05 2018
+
+@author: pjh523
+"""
+
+import gwy
+from pygwy_functions import threshold_otsu_median, set_mask_colour, \
+                            get_data_key, get_mask_key, create_mask
+
+plugin_menu = "/Grains/Batch/Otsu's (Median)"
+plugin_type = "PROCESS"
+plugin_desc = '''Equivalent to 'Otsu Modal Method -> Single, but works on all
+                open containers.'''
+
+def run():
+    # do thresholding on main (id=0) data
+    _id = 0
+    # get containers
+    containers = gwy.gwy_app_data_browser_get_containers()
+    # get current image data
+    for container in containers:
+        # get datafield and compute threshold
+        datafield = container.get_object_by_name(get_data_key(_id))
+        threshold = threshold_otsu_median(datafield)
+        # create grain mask
+        grains = create_mask(datafield, threshold)
+        # add mask field to container
+        container.set_object_by_name(get_mask_key(_id), grains)
+        
+        # set mask colour and opacity
+        set_mask_colour(container, _id)
