dea60598add6a4cdca337e13b7933fd76d9dd050
Paddy <pj.harrison31@gmail.com>
2019-11-05 18:37:29 +0000

    Added placeholder fn to alleviate bug

---
+++ do_nothing.py
@@ -0,0 +1,13 @@
+"""
+Created on Tue Nov 13 21:23:05 2018
+
+@author: pjh523
+"""
+
+plugin_menu = "/Do Nothing"
+plugin_type = "PROCESS"
+plugin_desc = """This function does nothing."""
+
+
+def run():
+    pass
