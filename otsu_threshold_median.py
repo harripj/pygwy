78302d2d48cfb4020663271f9c8562c937c6fa69
Paddy <pj.harrison31@gmail.com>
2019-11-05 18:37:09 +0000

    Black formatting

--- otsu_threshold_median.py
+++ otsu_threshold_median.py
@@ -6,13 +6,19 @@ Created on Tue Nov 13 21:23:05 2018
 
 import gwy
 import os
-from pygwy_functions import threshold_otsu_median, get_relative_value, \
-                            get_mask_key, get_data_key, create_mask, \
-                            set_mask_colour
+from pygwy_functions import (
+    threshold_otsu_median,
+    get_relative_value,
+    get_mask_key,
+    get_data_key,
+    create_mask,
+    set_mask_colour,
+)
 
-plugin_menu = "/Grains/Mark by Otsu\'s (Median)"
+plugin_menu = "/Grains/Mark by Otsu's (Median)"
 plugin_type = "PROCESS"
-plugin_desc = '''Mark grains in datafield by Otsu's method with a median descriptor.'''
+plugin_desc = """Mark grains in datafield by Otsu's method with a median descriptor."""
+
 
 def run():
     # get current data and correct container for file
@@ -21,7 +27,7 @@ def run():
 
     # create undo point
     gwy.gwy_undo_qcheckpoint(container, container.keys())
-    
+
     # get datafield and compute threshold
     datafield = container.get_object_by_name(get_data_key(_id))
     threshold = threshold_otsu_median(datafield)
@@ -29,6 +35,6 @@ def run():
     grains = create_mask(datafield, threshold)
     # add mask field to container
     container.set_object_by_name(get_mask_key(_id), grains)
-    
+
     # set mask colour and opacity
     set_mask_colour(container, _id)
