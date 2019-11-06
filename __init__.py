18140a1bc02a86ac27a43023d07394db8ab5e550
Paddy <pj.harrison31@gmail.com>
2019-11-06 15:25:21 +0000

    added __init__, do_nothing script

---
+++ __init__.py
@@ -0,0 +1,7 @@
+"""
+Created on Tue Nov 13 21:23:05 2018
+
+@author: pjh523
+"""
+
+# importable pygwy module
--- otsu_do_nothing.py
+++ otsu_do_nothing.py
@@ -4,10 +4,12 @@ Created on Tue Nov 13 21:23:05 2018
 @author: pjh523
 """
 
+import gwy
+
 plugin_menu = "/Do Nothing"
 plugin_type = "PROCESS"
 plugin_desc = """This function does nothing."""
 
 
 def run():
-    pass
+    print("This function literally does nothing.")
